7eb94ffcad1c78dcf9ebff923f51e91d5cc167c5
PaulG0 <90392507+PaulG0@users.noreply.github.com>
2021-11-25 16:22:11 +0100

    Add files via upload

--- php/td3php/td3php2.php
+++ php/td3php/td3php2.php
@@ -5,16 +5,28 @@ if (isset($_GET['logout'])) {
 	$_SESSION=[];
 	session_destroy();
 }
+if (isset($_COOKIE['user'])) {
+	$_SESSION['user']=$_COOKIE['user'];
+}
+
 if (isset($_POST['user'])) {
 	$_SESSION['user']=$_POST['user'];
+	if (isset($_POST['reco'])) {
+		$temps = 10;
+		setcookie ("user",$_POST['user'],time()+$temps);
+	}
 }
+
+
 if (isset($_SESSION['user'])) {
 	echo "vous etes connecté en tant que ".$_SESSION['user']."<br>";
 	echo"<a href='?logout'>deconnexion</a>"; 
+
 }else{
 	?>
 	<form action='<?=$_SERVER['PHP_SELF']?>' method='post'>
 		<input class="form-control" name="user" placeholder="saisir votre nom">
+		<input type="checkbox" name="reco"> rester connecter
 		<button class="btn btn-primary">valider</button>		
 	</form>
 <?php
